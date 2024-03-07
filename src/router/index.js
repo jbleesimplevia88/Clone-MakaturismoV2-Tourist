ee691a6f8c5994475d7e55db433d1780e48b5c44
FE-Luis <162388086+FE-Luis@users.noreply.github.com>
2024-03-07 15:31:11 +0800

    remove business routes

--- src/router/index.js
+++ src/router/index.js
@@ -27,61 +27,6 @@ const router = createRouter({
       component: TopPlace
     },
     {
-      path: '/partner',
-      name: 'Partner',
-      component: () => import('../views/BusinessView.vue'),
-      children: [
-        {
-          path: 'dashboard',
-          name: 'Dashboard',
-          component: () => import('@/components/Business/DashBoardContent.vue')
-        },
-        {
-          path: 'business',
-          name: 'Business',
-          component: () => import('@/components/Business/BusinessContent.vue')
-        },
-        {
-          path: 'transactions',
-          name: 'Transactions',
-          component: () => import('@/components/Business/TransactionsContent.vue')
-        },
-        {
-          path: 'terms',
-          name: 'T&C',
-          component: () => import('@/components/Business/T&cContent.vue')
-        },
-        {
-          path: 'information',
-          name: 'Informations',
-          component: () => import('@/components/Business/InformationContent.vue')
-        },
-        {
-          path: 'product-ratings',
-          name: 'Product Ratings',
-          component: () => import('@/components/Business/ProductRatings.vue')
-        },
-        {
-          path: 'sale-analytics',
-          name: 'Sale Analytics',
-          component: () => import('@/components/Business/SaleAnalytics.vue')
-        },
-        {
-          path: 'messages',
-          name: 'Messages',
-          component: () => import('@/components/Business/MessagesContent.vue')
-        },
-        {
-          path: 'product',
-          name: 'Product',
-          component: () => import('@/components/Business/ProductContent.vue')
-        },
-        
-        
-        // Other nested routes under Business if needed
-      ]
-    },
-    {
       path: '/todo',
       name: 'todo',
       component: () => import('../views/GuestView.vue')

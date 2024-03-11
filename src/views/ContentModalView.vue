b4241deb6da106feb22f07efbd2eaabb59ed7e18
Yvette Medrano <91317496+yvette0802@users.noreply.github.com>
2024-03-11 15:34:53 +0800

    Develop Content Modal for Places

---
+++ src/views/ContentModalView.vue
@@ -0,0 +1,13 @@
+<script setup>
+import NavBar from '@/components/NavBar.vue';
+import FooterPage from '@/components/FooterPage.vue';
+import ContentModal from '@/components/ContentModal.vue';
+
+</script>
+
+<template>
+    <NavBar />
+    <ContentModal />
+    <RouterView />
+    <FooterPage />
+</template>

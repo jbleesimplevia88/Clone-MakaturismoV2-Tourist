2a06cfca76bd2372334ea0623e9ec7d3fdd80dbb
FE-Luis <162388086+FE-Luis@users.noreply.github.com>
2024-03-14 22:13:34 +0800

    remove unused files

---
+++ src/views/CartView.vue
@@ -0,0 +1,14 @@
+<script setup>
+// import { RouterLink, RouterView } from 'vue-router'
+import NavBar from '@/components/NavBar.vue';
+import FooterPage from '@/components/FooterPage.vue';
+// import LoginModal from '@/components/LoginModal.vue';
+</script>
+
+<template>
+    <NavBar />
+    <RouterView />
+    <FooterPage />
+</template>
+
+<style scoped></style>

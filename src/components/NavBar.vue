246bc3f7916ff409bd4119de7d1ebdfe02fff7d4
jbleesimplevia <jblee.simplevia@gmail.com>
2024-03-26 14:29:34 +0800

    mobile responsive (filters, all categ)

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -13,7 +13,7 @@
       <div class="flex items-center space-x-4">
         <div class="relative flex " ref="searchContainer">
           <input v-if="showInput" type="search"
-            class=" relative m-0 block flex-auto rounded border border-solid border-neutral-200 bg-transparent bg-clip-padding px-3 py-[0.25rem] text-base font-normal leading-[1.6] text-surface outline-none transition duration-200 ease-in-out placeholder:text-neutral-500 focus:z-[3] focus:border-primary focus:shadow-inset focus:outline-none motion-reduce:transition-none dark:border-white/10 dark:text-white dark:placeholder:text-neutral-200 dark:autofill:shadow-autofill dark:focus:border-primary"
+            class=" relative m-0 block flex-auto rounded border border-solid border-neutral-200 bg-transparent bg-clip-padding px-3 py-[0.25rem] text-base font-normal leading-[1.6] text-surface outline-none transition duration-200 ease-in-out placeholder:text-neutral-500 focus:z-[3] focus:border-primary focus:shadow-inset focus:outline-none motion-reduce:transition-none"
             placeholder="Search" aria-label="Search" id="exampleFormControlInput2" aria-describedby="button-addon2"
             ref="searchInput" />
           <span @click="toggleInputVisibility"

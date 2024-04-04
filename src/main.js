fc6d7cc9bb3aecf8f2d63a54496fb9623df32bf7
FE-Luis <162388086+FE-Luis@users.noreply.github.com>
2024-04-04 10:09:07 +0800

    implement pinia

--- src/main.js
+++ src/main.js
@@ -1,12 +1,16 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
+import { createPinia } from 'pinia'
 import lazyloadDirective from './lazyload.directive.js' // Import the directive
 import './assets/index.css'
 
 const app = createApp(App)
+const pinia = createPinia()
 
 app.use(router)
+app.use(pinia)
+
 app.use(lazyloadDirective) // Use the lazyload directive
 
 app.mount('#app')

c2f93c2a9bd5b8d911cb02756532bb64cb7de327
FE-Luis <162388086+FE-Luis@users.noreply.github.com>
2024-04-05 13:29:08 +0800

    update the auth state

--- src/main.js
+++ src/main.js
@@ -3,6 +3,7 @@ import App from './App.vue'
 import router from './router'
 import { createPinia } from 'pinia'
 import lazyloadDirective from './lazyload.directive.js' // Import the directive
+import { useAuthStore } from './stores/auth';
 import './assets/index.css'
 
 const app = createApp(App)
@@ -10,6 +11,8 @@ const pinia = createPinia()
 
 app.use(router)
 app.use(pinia)
+
+useAuthStore().initialize();
 
 app.use(lazyloadDirective) // Use the lazyload directive
 

3f23c74b4b8d506b58445aabb3c84aa04dc1c911
jbleesimplevia <jblee.simplevia@gmail.com>
2024-04-16 10:02:51 +0800

    notif

---
+++ src/views/NotificationView.vue
@@ -0,0 +1,12 @@
+<script setup>
+import NavBar from '@/components/NavBar.vue';
+import FooterPage from '@/components/FooterPage.vue';
+import Notification from '@/components/NotificationContent.vue';
+
+</script>
+
+<template>
+    <NavBar />
+    <Notification />
+    <!-- <FooterPage /> -->
+</template>

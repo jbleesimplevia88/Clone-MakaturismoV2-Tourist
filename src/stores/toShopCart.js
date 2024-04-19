33060d449ffd6e80be1922f46228e16f6f5bebed
jbleesimplevia <jblee.simplevia@gmail.com>
2024-04-19 12:03:02 +0800

    WPRKING CART

--- src/stores/toShopCart.js
+++ src/stores/toShopCart.js
@@ -6,8 +6,8 @@ export const useCartStore = defineStore({
       cart: [] 
     }),
     actions: {
-      addToCart(product) {state
-        const existingProductIndex = this.$.cart.findIndex(item => item.title === product.title);
+      addToCart(product) {
+        const existingProductIndex = this.$state.cart.findIndex(item => item.title === product.title);
 
         if (existingProductIndex !== -1) {
           // If the product already exists, update the quantity

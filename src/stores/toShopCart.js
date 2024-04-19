c69104e23e588c24a4dfb709022daf3100e76535
jbleesimplevia <jblee.simplevia@gmail.com>
2024-04-19 12:00:13 +0800

    WORKING ADD TO CART

--- src/stores/toShopCart.js
+++ src/stores/toShopCart.js
@@ -1,30 +1,29 @@
 import { defineStore } from 'pinia';
 
 export const useCartStore = defineStore({
-  id: 'cart',
-  state: () => ({
-    cart: [] // Array to hold the products added to the cart
-  }),
-  actions: {
-    addToCart(product) {
-      const existingProductIndex = this.cart.findIndex(item => item.title === product.title);
+    id: 'cart',
+    state: () => ({
+      cart: [] 
+    }),
+    actions: {
+      addToCart(product) {state
+        const existingProductIndex = this.$.cart.findIndex(item => item.title === product.title);
 
-      if (existingProductIndex !== -1) {
-        // If the product already exists, update the quantity
-        this.cart[existingProductIndex].quantity += product.quantity;
-      } else {
-        // If it's a new product, add it to the cart
-        this.cart.unshift({
-          title: product.title,
-          quantity: product.quantity,
-          price: product.price
-        });
-      }
-    },
-    
-    // clearCart() {
-    //   // Clear the entire cart
-    //   this.cart = [];
-    // }
-  }
+        if (existingProductIndex !== -1) {
+          // If the product already exists, update the quantity
+          this.$state.cart[existingProductIndex].quantity += product.quantity;
+        } else {
+          // If it's a new product, add it to the cart
+          this.$state.cart.unshift({
+            title: product.title,
+            quantity: product.quantity,
+            price: product.price
+          });
+        }
+      },
+      // clearCart() {
+      //   // Clear the entire cart
+      //   this.cart = [];
+      // }
+    }
 });

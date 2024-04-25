b5c7f309d898ef30aa4cfe2bbce54cc994c169b9
jbleesimplevia <jblee.simplevia@gmail.com>
2024-04-25 11:46:29 +0800

    FINAL VOUCHER AND EDITCART

--- src/components/CartCheckoutShop.vue
+++ src/components/CartCheckoutShop.vue
@@ -44,7 +44,7 @@
                             </div>
                         </div>
                         <!-- Information of user -->
-                        <div class="relative hidden lg:block mx-6 px-3 lg:pl-32 mb-[30px] lg:mb-24">
+                        <div class="relative hidden lg:block mx-6 px-3 lg:pl-32 mb-[30px] lg:mb-44">
                             <p class="mb-4 font-bold lg:text-3xl text-2xl">Your Information</p>
                             <div class="grid grid-cols-1 lg:grid-cols-2 lg:gap-4">
                                 <div class="flex lg:flex-col lg:items-start justify-start">
@@ -116,10 +116,10 @@
                             </div>
                         </div>
                     </div>
-                    
+
                     <!-- Start of Central Business District Tour -->
                     <!-- <div v-for="(shop, index) in shops" :key="index" class="my-4 lg:w-[32%] lg:h-[30%] lg:right-10 lg:absolute relative lg:top-[3rem] w-screen"> -->
-                        <!-- <div class="border border-gray-400 rounded-xl p-5 ml-5 w-[90%] h-fit"> -->
+                    <!-- <div class="border border-gray-400 rounded-xl p-5 ml-5 w-[90%] h-fit"> -->
 
                     <div class="my-4 lg:w-[32%] lg:h-[30%] lg:right-10 lg:absolute relative lg:top-[3rem] w-screen">
                         <div class="border border-gray-400 rounded-xl p-5 ml-5 w-[90%] h-fit">
@@ -142,14 +142,17 @@
                                         </div>
                                     </div>
                                     <div class="bg-gray-400 h-0.5"></div>
-                                    <p class="lg:mr-9 lg:mt-2 lg:font-sans lg:text-3xl font-bold text-lg pt-4">Your Total</p>
+                                    <p class="lg:mr-9 lg:mt-2 lg:font-sans lg:text-3xl font-bold text-lg pt-4">Your Total
+                                    </p>
                                     <div class="items-center mb-5">
-                                        <div v-for="(cartItem, index) in cart" :key="index" class="flex justify-between mb-2 w-[100%]">
+                                        <div v-for="(cartItem, index) in selectedItems" :key="index"
+                                            class="flex justify-between mb-2 w-[100%]">
                                             <p class="w-[70%]">{{ cartItem.title }} </p>
                                             <p class="text-gray-400">₱ {{ cartItem.quantity * cartItem.price }}</p>
                                         </div>
 
-                                        <p class="lg:font-poppins font-sans text-base font-bold text-right underline hidden">
+                                        <p
+                                            class="lg:font-poppins font-sans text-base font-bold text-right underline hidden">
                                             Price Breakdown
                                         </p>
                                     </div>
@@ -157,20 +160,23 @@
                                     <div class="bg-gray-400 h-0.5 w-[100%]"></div>
                                     <div class="flex items-center my-7  ">
                                         <img src="@/assets/images/CategoryView/ToShop/voucher.png" class="lg:w-8 h-8 mr-2">
-                                        <button class="hidden lg:block text-white bg-blue-500 rounded-xl px-3 py-2 text-xs font-semibold"
+                                        <button
+                                            class="hidden lg:block text-white bg-blue-500 rounded-xl px-3 py-2 text-xs font-semibold"
                                             @click="toggleVoucherWeb">Use Voucher</button>
-                                        <button class="lg:hidden text-white bg-blue-500 rounded-xl px-3 py-2 text-xs font-semibold"
+                                        <button
+                                            class="lg:hidden text-white bg-blue-500 rounded-xl px-3 py-2 text-xs font-semibold"
                                             @click="toggleVoucher">Use Voucher</button>
                                     </div>
                                     <div class="flex justify-between">
-                                    <!-- Change "Your Total (Php)" to "Subtotal" if a voucher is applied -->
+                                        <!-- Change "Your Total (Php)" to "Subtotal" if a voucher is applied -->
                                         <p class="font-poppins font-sans font-bold text-lg pt-4">{{ displayTotalLabel }}</p>
                                         <p class="font-poppins font-sans text-base font-bold pt-4">{{ totalAmount }}</p>
                                     </div>
-                                <!-- If the discountPrice has a value, show this line -->
+                                    <!-- If the discountPrice has a value, show this line -->
                                     <div v-if="discountPrice !== 0" class="flex justify-between text-[#9bbf2f]">
                                         <p class="font-poppins font-sans font-bold text-lg pt-4">Special Discount:</p>
-                                        <p class="font-poppins font-sans text-base font-bold pt-4">- ₱ {{ discountPrice }} </p>
+                                        <p class="font-poppins font-sans text-base font-bold pt-4">- ₱ {{ discountPrice }}
+                                        </p>
                                     </div>
 
                                     <div class="bg-gray-400 h-0.5 mt-2"></div>
@@ -179,7 +185,8 @@
                                         <p class="font-poppins font-sans font-bold text-lg pt-4">Total (in PHP):</p>
                                         <p class="font-poppins font-sans text-xl font-bold pt-4">{{ finalPrice }}</p>
                                     </div>
-                                    <p class="lg:font-poppins font-sans text-base font-bold text-right underline hidden">Price
+                                    <p class="lg:font-poppins font-sans text-base font-bold text-right underline hidden">
+                                        Price
                                         Breakdown
                                     </p>
                                     <!-- For Mobile -->
@@ -226,14 +233,15 @@
                             </div>
 
                             <div v-else>
-                               <div class="lg:w-[100%]  rounded-lg">
+                                <div class="lg:w-[100%]  rounded-lg">
                                     <!-- Voucher section -->
                                     <!-- <button class="text-white bg-blue-500 rounded-xl px-3 py-2 text-xs font-semibold" @click="toggleBack">Back</button> -->
-                                    <svg @click="toggleBack" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
-                                        stroke-width="3" stroke="black" class="lg:mr-5 lg:w-5 lg:h-10">
-                                        <path stroke-linecap="round" stroke-linejoin="round" d="M10.5 19.5 3 12m0 0 7.5-7.5M3 12h18">
+                                    <svg @click="toggleBack" xmlns="http://www.w3.org/2000/svg" fill="none"
+                                        viewBox="0 0 24 24" stroke-width="3" stroke="black" class="lg:mr-5 lg:w-5 lg:h-10">
+                                        <path stroke-linecap="round" stroke-linejoin="round"
+                                            d="M10.5 19.5 3 12m0 0 7.5-7.5M3 12h18">
                                         </path>
-                                    </svg> 
+                                    </svg>
                                     <div class="flex flex-col items-center">
                                         <input type="text" id="" name="" value=""
                                             class="mb-2 h-[50px] w-[100%] border border-gray-200 pl-5 pr-3 rounded-md"
@@ -249,7 +257,8 @@
                                             <div class="absolute grid grid-rows-3 text-left ml-14 mt-1">
                                                 <span class="font-semibold">{{ voucher.code }}</span>
                                                 <span class="font-bold">P{{ voucher.amount }}</span>
-                                                <button type="submit" class="text-sm font-bold ml-52 cursor-pointer" @click="toggleVoucherWeb(voucher)">
+                                                <button type="submit" class="text-sm font-bold ml-52 cursor-pointer"
+                                                    @click="toggleVoucherWeb(voucher)">
                                                     {{ voucher.applied ? 'Remove' : 'Apply' }}
                                                 </button>
 
@@ -265,7 +274,8 @@
                                             <div class="absolute grid grid-rows-3 text-left ml-14 mt-1">
                                                 <span class="font-semibold">{{ voucher.code }}</span>
                                                 <span class="font-bold">P{{ voucher.amount }}</span>
-                                                <button type="submit" class="text-sm font-bold ml-52 cursor-pointer" disabled>
+                                                <button type="submit" class="text-sm font-bold ml-52 cursor-pointer"
+                                                    disabled>
                                                     Apply
                                                 </button>
                                             </div>
@@ -274,12 +284,12 @@
                                     </div>
                                 </div>
                             </div>
-                           
+
                         </div>
                     </div>
                 </div>
             </template>
-            
+
             <!-- Monile | Proceed to payment -->
             <template v-else>
                 <div class="lg:hidden">
@@ -337,7 +347,7 @@
         </div>
     </template>
 
-    <template v-else> 
+    <template v-else>
         <div class="lg:w-[100%] p-4 rounded-lg text-center flexflex-col items-center pt-20">
             <!-- Voucher section -->
             <div class="flex flex-col items-center">

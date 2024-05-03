68c8181f9f4943a6a17363883a7baf61c41cc9be
anne2320 <159417044+anne2320@users.noreply.github.com>
2024-05-03 10:21:36 +0800

    initial test

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -1071,108 +1071,90 @@
             </div>
         </div>
 
-        <div v-if="isCategoryPath($route.path)"
-            class="hidden lg:inset-x-0 lg:bottom-0 lg:grid lg:grid-cols-6 lg:pl-10 lg:pr-10 lg:pt-2 lg:pb-2 lg:justify-center lg:text-black lg:bg-white lg:border-t max-w-full">
-            <!-- Location 1 -->
-            <div class="m-location-card lg:m-0 m-[0] w-[100%] h-auto lg:location-card flex items-center rounded-l-lg"
-                :class="['location-card', locations[0].attrib, { 'active-effect': isActive(locations[0].link) }]">
-                <RouterLink :to="locations[0].link">
+    <div v-if="isCategoryPath($route.path)" class="hidden lg:inset-x-0 lg:bottom-0 lg:grid lg:grid-cols-6 lg:pl-20 lg:pr-20 lg:pt-2 lg:pb-2 lg:justify-center lg:text-black lg:bg-white lg:border-t  max-w-full">
+        <!-- Location 1 -->
+        <div class="m-location-card lg:m-0 m-[0] w-[100%] h-auto lg:location-card flex items-center rounded-l-lg" :class="['location-card', locations[0].attrib, { 'active-effect': isActive(locations[0].link) }]">
+            <RouterLink :to="locations[0].link">
+                <!-- Adjust height as needed -->
+                <div class="flex items-center justify-center h-10 max-w-full max-h-full">
+                    <img :src="locations[0].imgSrc" :alt="locations[0].alt" class="p-[26px] max-w-full max-h-[230px]">
                     <!-- Adjust height as needed -->
-                    <div class="flex items-center justify-center h-10 max-w-full max-h-full">
-                        <img :src="locations[0].imgSrc" :alt="locations[0].alt"
-                            class="p-[26px] max-w-full max-h-[260px]">
-                        <!-- Adjust height as needed -->
-                        <span class="text-center lg:my-3 lg:font-bold lg:text-1xl lg:hidden" style="margin: 0;">{{
-                            locations[0].mobile }}</span>
-                        <span class="font-bold text-1xl">{{ locations[0].mobile }}</span>
-                    </div>
-                </RouterLink>
-            </div>
+                    <span class="text-center lg:my-3 lg:font-bold lg:text-1xl lg:hidden " style="margin: 0;">{{
+            locations[0].mobile }}</span>
+                    <span class="font-bold text-1xl lg:-ml-5">{{ locations[0].mobile }}</span>
+                </div>
+            </RouterLink>
+        </div>
 
-            <!-- Location 2 -->
-            <div class="m-location-card lg:m-0 m-[0] w-[100%] h-auto lg:location-card flex items-center"
-                :class="['location-card', locations[1].attrib, { 'active-effect': isActive(locations[1].link) }]"
-                style="margin: 0; width: 100%; height: auto;">
-                <RouterLink :to="locations[1].link">
+        <!-- Location 2 -->
+        <div class="m-location-card lg:m-0 m-[0] w-[100%] h-auto lg:location-card flex items-center" :class="['location-card', locations[1].attrib, { 'active-effect': isActive(locations[1].link) }]" style="margin: 0; width: 100%; height: auto;">
+            <RouterLink :to="locations[1].link">
+                <!-- Adjust height as needed -->
+                <div class="flex items-center justify-center h-10">
+                    <img :src="locations[1].imgSrc" :alt="locations[1].alt" class="p-[26px] max-w-full max-h-[180px]">
                     <!-- Adjust height as needed -->
-                    <div class="flex items-center justify-center h-10">
-                        <img :src="locations[1].imgSrc" :alt="locations[1].alt"
-                            class="p-[26px] max-w-full max-h-[220px]">
-                        <!-- Adjust height as needed -->
-                        <span class="text-center lg:my-3 lg:font-bold lg:text-1xl lg:hidden" style="margin: 0;">{{
-                            locations[1].mobile }}</span>
-                        <span class="font-bold text-1xl">{{ locations[1].mobile }}</span>
-                    </div>
-                </RouterLink>
-            </div>
+                    <span class="text-center lg:my-3 lg:font-bold lg:text-1xl lg:hidden" style="margin: 0;">{{
+            locations[1].mobile }}</span>
+                    <span class="font-bold text-1xl lg:-ml-2">{{ locations[1].mobile }}</span>
+                </div>
+            </RouterLink>
+        </div>
 
-            <!-- Location 3 -->
-            <div class="m-location-card lg:m-0 m-[0] w-[100%] h-auto lg:location-card flex items-center"
-                :class="['location-card', locations[2].attrib, { 'active-effect': isActive(locations[2].link) }]"
-                style="margin: 0; width: 100%; height: auto;">
-                <RouterLink :to="locations[2].link">
+        <!-- Location 3 -->
+        <div class="m-location-card lg:m-0 m-[0] w-[100%] h-auto lg:location-card flex items-center" :class="['location-card', locations[2].attrib, { 'active-effect': isActive(locations[2].link) }]" style="margin: 0; width: 100%; height: auto;">
+            <RouterLink :to="locations[2].link">
+                <!-- Adjust height as needed -->
+                <div class="flex items-center justify-center h-10">
+                    <img :src="locations[2].imgSrc" :alt="locations[2].alt" class="p-[26px] max-w-full max-h-[200px]">
                     <!-- Adjust height as needed -->
-                    <div class="flex items-center justify-center h-10">
-                        <img :src="locations[2].imgSrc" :alt="locations[2].alt"
-                            class="p-[26px] max-w-full max-h-[265px]">
-                        <!-- Adjust height as needed -->
-                        <span class="text-center lg:my-3 lg:font-bold lg:text-1xl lg:hidden" style="margin: 0;">{{
-                            locations[2].mobile }}</span>
-                        <span class="font-bold text-1xl">{{ locations[2].mobile }}</span>
-                    </div>
-                </RouterLink>
-            </div>
+                    <span class="text-center lg:my-3 lg:font-bold lg:text-1xl lg:hidden" style="margin: 0;">{{
+            locations[2].mobile }}</span>
+                    <span class="font-bold text-1xl lg:-ml-4">{{ locations[2].mobile }}</span>
+                </div>
+            </RouterLink>
+        </div>
 
-            <!-- Location 4 -->
-            <div class="m-location-card lg:m-0 m-[0] w-[100%] h-auto lg:location-card flex items-center"
-                :class="['location-card', locations[3].attrib, { 'active-effect': isActive(locations[3].link) }]"
-                style="margin: 0; width: 100%; height: auto;">
-                <RouterLink :to="locations[3].link">
+        <!-- Location 4 -->
+        <div class="m-location-card lg:m-0 m-[0] w-[100%] h-auto lg:location-card flex items-center" :class="['location-card', locations[3].attrib, { 'active-effect': isActive(locations[3].link) }]" style="margin: 0; width: 100%; height: auto;">
+            <RouterLink :to="locations[3].link">
+                <!-- Adjust height as needed -->
+                <div class="flex items-center justify-center h-10">
+                    <img :src="locations[3].imgSrc" :alt="locations[3].alt" class="p-[26px] max-w-full max-h-[200px]">
                     <!-- Adjust height as needed -->
-                    <div class="flex items-center justify-center h-10">
-                        <img :src="locations[3].imgSrc" :alt="locations[3].alt"
-                            class="p-[26px] max-w-full max-h-[240px]">
-                        <!-- Adjust height as needed -->
-                        <span class="text-center lg:my-3 lg:font-bold lg:text-1xl lg:hidden" style="margin: 0;">{{
-                            locations[3].mobile }}</span>
-                        <span class="font-bold text-1xl">{{ locations[3].mobile }}</span>
-                    </div>
-                </RouterLink>
-            </div>
+                    <span class="text-center lg:my-3 lg:font-bold lg:text-1xl lg:hidden" style="margin: 0;">{{
+            locations[3].mobile }}</span>
+                    <span class="font-bold text-1xl lg:-ml-4">{{ locations[3].mobile }}</span>
+                </div>
+            </RouterLink>
+        </div>
 
-            <!-- Location 5 -->
-            <div class="m-location-card lg:m-0 m-[0] w-[100%] h-auto lg:location-card flex items-center"
-                :class="['location-card', locations[4].attrib, { 'active-effect': isActive(locations[4].link) }]"
-                style="margin: 0; width: 100%; height: auto;">
-                <RouterLink :to="locations[4].link">
+        <!-- Location 5 -->
+        <div class="m-location-card lg:m-0 m-[0] w-[100%] h-auto lg:location-card flex items-center" :class="['location-card', locations[4].attrib, { 'active-effect': isActive(locations[4].link) }]" style="margin: 0; width: 100%; height: auto;">
+            <RouterLink :to="locations[4].link">
+                <!-- Adjust height as needed -->
+                <div class="flex items-center justify-center h-10">
+                    <img :src="locations[4].imgSrc" :alt="locations[4].alt" class="p-[26px] max-w-full max-h-[190px]">
                     <!-- Adjust height as needed -->
-                    <div class="flex items-center justify-center h-10">
-                        <img :src="locations[4].imgSrc" :alt="locations[4].alt"
-                            class="p-[26px] max-w-full max-h-[232px]">
-                        <!-- Adjust height as needed -->
-                        <span class="text-center lg:my-3 lg:font-bold lg:text-1xl lg:hidden" style="margin: 0;">{{
-                            locations[4].mobile }}</span>
-                        <span class="font-bold text-1xl">{{ locations[4].mobile }}</span>
-                    </div>
-                </RouterLink>
-            </div>
+                    <span class="text-center lg:my-3 lg:font-bold lg:text-1xl lg:hidden" style="margin: 0;">{{
+            locations[4].mobile }}</span>
+                    <span class="font-bold text-1xl lg:-ml-4">{{ locations[4].mobile }}</span>
+                </div>
+            </RouterLink>
+        </div>
 
-            <!-- Location 6 -->
-            <div class="m-location-card lg:m-0 m-[0] w-[100%] h-auto lg:location-card flex items-center rounded-r-lg"
-                :class="['location-card', locations[5].attrib, { 'active-effect': isActive(locations[5].link) }]"
-                style="margin: 0; width: 100%; height: auto;">
-                <RouterLink :to="locations[5].link">
+        <!-- Location 6 -->
+        <div class="m-location-card lg:m-0 m-[0] w-[100%] h-auto lg:location-card flex items-center rounded-r-lg" :class="['location-card', locations[5].attrib, { 'active-effect': isActive(locations[5].link) }]" style="margin: 0; width: 100%; height: auto;">
+            <RouterLink :to="locations[5].link">
+                <!-- Adjust height as needed -->
+                <div class="flex items-center justify-center h-10">
+                    <img :src="locations[5].imgSrc" :alt="locations[5].alt" class="p-[26px] max-w-full max-h-[210px]">
                     <!-- Adjust height as needed -->
-                    <div class="flex items-center justify-center h-10">
-                        <img :src="locations[5].imgSrc" :alt="locations[5].alt"
-                            class="p-[26px] max-w-full max-h-[275px]">
-                        <!-- Adjust height as needed -->
-                        <span class="text-center lg:my-3 lg:font-bold lg:text-1xl lg:hidden" style="margin: 0;">{{
-                            locations[5].mobile }}</span>
-                        <span class="font-bold text-1xl">{{ locations[5].mobile }}</span>
-                    </div>
-                </RouterLink>
-            </div>
+                    <span class="text-center lg:my-3 lg:font-bold lg:text-1xl lg:hidden" style="margin: 0;">{{
+            locations[5].mobile }}</span>
+                    <span class="font-bold text-1x lg:-ml-4">{{ locations[5].mobile }}</span>
+                </div>
+            </RouterLink>
+        </div>
 
             <!-- Repeat the above structure for each location, changing the indices accordingly -->
         </div>
